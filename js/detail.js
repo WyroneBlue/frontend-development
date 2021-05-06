b9e0b4754cd1fbe88b76c8059c633c5fbcaa81aa
Ymaro <ymaro1998@gmail.com>
2021-05-06 16:37:34 +0200

    changed shoecolors selection

--- js/detail.js
+++ js/detail.js
@@ -7,7 +7,7 @@ var names = document.querySelectorAll(".heading h1");
 var forWho = document.querySelectorAll(".heading h2");
 var prices = document.querySelectorAll(".heading span");
 var images = document.querySelector(".images div");
-var colors = document.querySelector("#shoe-colors");
+var colors = document.querySelector("#shoe-colors div");
 
 var html = {
     image: '',
@@ -31,17 +31,18 @@ let getCurrentShoe = function(e) {
     let currentShoeImages = currentShoe.images;
     for (const key in currentShoeImages) {
 
-        let image = currentShoeImages[key];        
+        let image = currentShoeImages[key];  
         html.image += `<figure><img src="${image}" alt=""></figure>`;
     }
     images.innerHTML = html.image;
 
     let currentcolors = currentShoe.colors;
     for (const key in currentcolors) {
-        let colorImage = currentcolors[key].image;        
-        html.colors += `<a href="#"><img src="${colorImage}" alt=""></a>`;
+        let colorImage = currentcolors[key].image;
+        let empty = (currentcolors[key].in_stock != true) ? 'class="empty"' : '';        
+        html.colors += `<a href=""><img ${empty} src="${colorImage}" alt=""></a>`;
     }
-    colors.innerHTML = html.colors;
+    colors.innerHTML += html.colors;
 }
 
 window.addEventListener('DOMContentLoaded', getCurrentShoe);
