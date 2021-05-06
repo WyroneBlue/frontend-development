688e8b902748a4070d3570566f2d2a9fb4410fcf
Ymaro <ymaro1998@gmail.com>
2021-05-06 15:50:05 +0200

    get cart selection from html

--- js/cart.js
+++ js/cart.js
@@ -5,12 +5,13 @@ var popup = document.querySelector("#cart-popup");
 var popupCloser = document.querySelector("#cart-popup .close");
 
 var cart = {
-    name: '',
-    for: '',
-    size: '',
-    image: '',
+    msg: document.querySelector('#cart-popup > span:first-of-type'),
+    image: document.querySelector('#cart-popup > section > img'),
+    name: document.querySelector('#cart-popup > section > div > span:nth-child(1)'),
+    for: document.querySelector('#cart-popup > section > div > span:nth-child(2)'),
+    size: document.querySelector('#cart-popup > section > div > span:nth-child(3)'),
+    price: document.querySelector('#cart-popup > section > div > span:nth-child(4)'),
 }
-
 
 const showCartPopup = function(e){
     
